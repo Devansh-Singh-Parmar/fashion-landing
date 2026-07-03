bbce3d9947d46cc63b012f14d64f2dd6acf9aba4
Devansh Singh Parmar <devansh18dp@gmail.com>
2026-07-03 14:10:23 +0530

    routes base

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
 
-  basePath: "/",
+  basePath: "/fashion",
 
   assetPrefix: "https://zineps-landing-pages.vercel.app/",
 
